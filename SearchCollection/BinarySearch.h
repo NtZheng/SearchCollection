a07ed0c450795e9ff856170d607d89bf25f82812
NtZheng <2745674085@qq.com>
2017-04-02 13:23:16 +0800

    完成了插值查找和斐波那契查找

--- SearchCollection/BinarySearch.h
+++ SearchCollection/BinarySearch.h
@@ -9,6 +9,13 @@
 #import <Foundation/Foundation.h>
 
 @interface BinarySearch : NSObject
+/*
+ * 插值查找：将key到start的大小和end到start的大小做比例，对mid做处理
+ *         如果数据均匀，优与二分
+ *         如果数据极端，差别很大，劣与二分
+ * 斐波那契查找：利用斐波那契数据对mid做处理
+ *
+ */
 
 + (int)binarySearchFor :(NSNumber *)key InArray :(NSMutableArray *)array;
 
