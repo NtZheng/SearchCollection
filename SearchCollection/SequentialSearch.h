3993273dddd61045abd9239165ac8d88b4be7789
NtZheng <2745674085@qq.com>
2017-04-02 10:34:57 +0800

    建立了项目，开始第一个算法，顺序查找

---
+++ SearchCollection/SequentialSearch.h
@@ -0,0 +1,13 @@
+//
+//  SequentialSearch.h
+//  SearchCollection
+//
+//  Created by nineteen on 4/2/17.
+//  Copyright © 2017 nineteen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SequentialSearch : NSObject
+
+@end
---
+++ SearchCollection/SequentialSearch.m
@@ -0,0 +1,13 @@
+//
+//  SequentialSearch.m
+//  SearchCollection
+//
+//  Created by nineteen on 4/2/17.
+//  Copyright © 2017 nineteen. All rights reserved.
+//
+
+#import "SequentialSearch.h"
+
+@implementation SequentialSearch
+
+@end
