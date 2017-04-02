4c4f140858636465d3114f3b2d61b56e918dee54
NtZheng <2745674085@qq.com>
2017-04-02 10:32:21 +0800

    Initial Commit

---
+++ SearchCollection/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SearchCollection
+//
+//  Created by nineteen on 4/2/17.
+//  Copyright © 2017 nineteen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
